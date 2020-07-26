ef3d571a09bf046b5982ad3f75c266d08b1c4fd7
Tiago Leite <tiago.silvaleite@gmail.com>
2020-07-26 10:54:06 -0300

    Asynchronous operations using Task (async, await)

--- SalesWebMvc1/Services/DepartmentService.cs
+++ SalesWebMvc1/Services/DepartmentService.cs
@@ -1,22 +1,27 @@
-﻿using SalesWebMvc1.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc1.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SalesWebMvc1.Services
 {
     public class DepartmentService
     {
-        // dependencia
+        // Injeção de dependencia
         private readonly SalesWebMvc1Context _context;
-
         public DepartmentService(SalesWebMvc1Context context)
         {
             _context = context;
         }
 
-        public List<Department> FindAll()
+        /// <summary>
+        /// FindAllAsync
+        /// </summary>
+        /// <returns>Lista assincrona</returns>
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
